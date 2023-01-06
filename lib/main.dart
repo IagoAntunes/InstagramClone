6a2e7f81cbb578f2f6e9099d5591bbb55374fdfd
IagoAntunes <iagoantunes.f@gmail.com>
2023-01-06 14:47:49 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagramclone/responsive/mobile_screen_layout.dart';
+import 'package:instagramclone/responsive/responsive_layout_screen.dart';
+import 'package:instagramclone/responsive/web_screen_layout.dart';
+import 'package:instagramclone/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAQCwF_EybwNpNd3d8BXutsbdu9Z-ekkiU",
+        appId: "1:935044049371:web:7a3d4228b40e9aa08adffe",
+        messagingSenderId: "935044049371",
+        projectId: "instagramclone-a1bba",
+        storageBucket: "instagramclone-a1bba.appspot.com",
+      ),
+    );
+  }
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.light()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagramclone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    super.key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  });
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > WebScreenSize) {
+          return webScreenLayout;
+        } else {
+          return mobileScreenLayout;
+        }
+      },
+    );
+  }
+}
