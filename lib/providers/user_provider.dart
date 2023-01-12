7fde91733561b408aa6b0440e0e87197d4d309b7
IagoAntunes <iagoantunes.f@gmail.com>
2023-01-12 10:14:32 -0300

    BottomNavigationBar

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:instagramclone/models/user.dart';
 import 'package:instagramclone/resources/auth_methods.dart';
 
-class UserProvider extends ChangeNotifier {
+class UserProvider with ChangeNotifier {
   User? _user;
+
   final AuthMethos _authMethods = AuthMethos();
-  User get getUser => _user!;
+
+  User? get getUser => _user;
 
   Future<void> refreshUser() async {
     User user = await _authMethods.getUserDetails();
+
     _user = user;
+
     notifyListeners();
   }
 }
---
+++ lib/responsive/screens/comment_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagramclone/utils/colors.dart';
+
+class CommentsScreen extends StatefulWidget {
+  const CommentsScreen({super.key});
+
+  @override
+  State<CommentsScreen> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: const Text('Comments'),
+        centerTitle: false,
+      ),
+    );
+  }
+}
